ee7a97141f14735c20cef100c98a0b3f5baf3d71
ohinhyuk <8156217@naver.com>
2023-01-07 02:33:06 +0900

    algorithm practice
    binary search

--- 1300_오인혁_20230105.cpp
+++ 1300_오인혁_20230105.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
 using namespace std;
- int N,k, mid;
-int calculate(int y){
-
-    int x = 0 ;
-
-    while(y < N){
-        y -= N;
-        x++;
+ long long N,k, mid;
+long long calculate(long long mid){
+    long long cnt = 0;
+    for(int i = 1 ; i <=N; ++i){
+        cnt += min(N, mid/i);
+        if(mid/i == 0) break;
     }
 
-    return x * y;
+    return cnt;
 
 }
 
@@ -20,21 +18,23 @@ int main(int argc, char** argv){
 
     cin >> N >> k;
 
-    int left = 1;
-    int right = N * N;
-    pair<int,int> p = make_pair(N,N);
+    long long left = 1;
+    long long right = N*N;
+    long long ans = 0;
     
-    while(left < right){
-        mid = (left+ right)/2;
-        
-        if(mid > k){
-            right = mid - 1;
+    while(left <= right){
+        long long mid = (left+right)/2;
+
+        long long cnt = calculate(mid);
+
+        if(cnt < k) left = mid + 1;
+        else if(cnt >= k){
+            ans = mid;
+            right = mid -1;
             
-            }
-        else if(mid < k){
-             left = mid + 1;
-             }
-        else break;
+        } 
+        
     }
-    cout << mid-1 << "\n";
+    cout << ans;
+
 }
