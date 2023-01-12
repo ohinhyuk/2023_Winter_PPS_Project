c518f4b6f4c2e3d38c5c3a70c0d111e2504b4362
ohinhyuk <8156217@naver.com>
2023-01-12 20:54:03 +0900

    algorithm study

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc ,char** argv){
+    string s;
+
+    cin >> s;
+
+    string d;
+
+    d = s;
+
+    s = "111";
+
+    cout << d << s;
+}
